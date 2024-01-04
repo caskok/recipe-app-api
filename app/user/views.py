5ad46a5122e45d24786f862c6ce983909ebca64f
Cas Kok <cas.kok@ordina.nl>
2024-01-04 11:18:01 +0100

    Add user api and implement token authorization

---
+++ app/user/views.py
@@ -0,0 +1,20 @@
+"""Views for the user API."""
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for users."""
+    serializer_class = AuthTokenSerializer
+    renderer_clases = api_settings.DEFAULT_RENDERER_CLASSES
